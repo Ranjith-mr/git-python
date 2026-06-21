d1b8fa3a6e7ac98dfa920af801c46a7b7872ebb5
ranjith <ranjith123@gmail.com>
2026-06-21 21:30:47 +0530

    commiting code

---
+++ calculator.py
@@ -0,0 +1,12 @@
+a=float(input("enter first number :"))
+op=input("enter operation(+,-,*,/))")
+b=float(input("enter second number"))
+
+if op=="+":
+  print("result",a+b)
+elif op=="-":
+  print("result",a-b)
+elif op=="*":
+  print("result",a*b)
+elif op=="/":
+  print("result",a/b)
