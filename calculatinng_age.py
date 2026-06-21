37f02ef76b7f4717806bc8927dd2757bbfabd2a4
ranjith <ranjith123@gmail.com>
2026-06-21 22:47:28 +0530

    commiting code2

---
+++ calculatinng_age.py
@@ -0,0 +1,7 @@
+#finding age without using date function and 
+birth_year=int(input("enter your birth year: "))
+current_year=2026#are hould direct enter current year like 2026 
+age=current_year-birth_year
+print("your age is ",age)
+
+
